27a2cbed78d0ffbd64751211546d5c4caf89f233
douglasneves-net <d88.neves@gmail.com>
2022-06-01 20:34:39 -0300

    feat: pages
    pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
 
 export default function App() {
+
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>Pagina Home</Text>
+      <Button title="Detalhes" onPress={() => navigation.navigate("Detail")} />
     </View>
   )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './pages/Home';
+import Detail from './pages/Detail';
+import CategoryPosts from './pages/CategoryPosts';
+import Search from './pages/Search';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +13,42 @@ function Routes() {
       <Stack.Screen
         name="Home"
         component={Home}
+        options={{
+          headerShown: false
+        }}
+      />
+      <Stack.Screen
+        name="Detail"
+        component={Detail}
+        options={{
+          title: "Detalhes",
+          headerTintColor: '#fff',
+          headerStyle: {
+            backgroundColor: "#232630"
+          }
+        }}
+      />
+      <Stack.Screen
+        name="CategoryPosts"
+        component={CategoryPosts}
+        options={{
+          title: "Categorias",
+          headerTintColor: '#fff',
+          headerStyle: {
+            backgroundColor: "#232630"
+          }
+        }}
+      />
+      <Stack.Screen
+        name="Search"
+        component={Search}
+        options={{
+          title: "Procurando algo? ",
+          headerTintColor: '#fff',
+          headerStyle: {
+            backgroundColor: "#232630"
+          }
+        }}
       />
     </Stack.Navigator>
   )
